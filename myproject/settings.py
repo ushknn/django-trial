32e96d260d9994d1dadf01f18bc1666cae148f6f
ushknn <uekiworks@gmail.com>
2024-11-03 19:17:11 +0900

    db setting from mysql to postgresql

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgressql',
+        'NAME': 'testdb',
+        'USER': 'testuser',
+        'PASSWORD': 'testpass',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
